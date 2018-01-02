01922c922efb5fa25667869fe0af37202e4ef8e5
davidraj <davidraj.samuel@gmail.com>
2018-01-02 16:58:08 +0000

    error in link fixed

--- lib/companies_house_xmlgateway/service/director_resignation.rb
+++ lib/companies_house_xmlgateway/service/director_resignation.rb
@@ -2,7 +2,7 @@ module CompaniesHouseXmlgateway
   module Service
     class DirectorResignation < CompaniesHouseXmlgateway::Service::Form
       API_CLASS_NAME = 'OfficerResignation'
-      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v1-0/schema/forms/...'
+      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v1-0/schema/forms/OfficerResignation-v2-6.xsd'
       
       # Generate the XML document that is embedded in the envelope for a Director Resignation action
       def build(submission)
