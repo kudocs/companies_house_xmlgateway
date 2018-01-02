b51a512664cab95404539d0a03db821ddbfc96ee
davidraj <davidraj.samuel@gmail.com>
2018-01-02 17:18:55 +0000

    issues fixed for secretary resignation

--- lib/companies_house_xmlgateway/service/secretary_resignation.rb
+++ lib/companies_house_xmlgateway/service/secretary_resignation.rb
@@ -2,12 +2,12 @@ module CompaniesHouseXmlgateway
   module Service
     class SecretaryResignation < CompaniesHouseXmlgateway::Service::Form
       API_CLASS_NAME = 'OfficerResignation'
-      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v1-0/schema/forms/...'
+      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v1-0/schema/forms/OfficerResignation-v2-6.xsd'
       
       # Generate the XML document that is embedded in the envelope for a Secretary Resignation action
       def build(submission)
         super do |xml|
-          xml.ConfirmationStatement(
+          xml.OfficerResignation(
             'xmlns' => 'http://xmlgw.companieshouse.gov.uk',
             'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
             'xsi:schemaLocation' => "http://xmlgw.companieshouse.gov.uk #{SCHEMA_XSD}"
@@ -19,8 +19,7 @@ module CompaniesHouseXmlgateway
               end
               xml.Person do
                 xml.Forename submission.data[:forename]
-                xml.Surname submission.data[:surname]
-                xml.DOB submission.data[:dob]
+                xml.Surname submission.data[:surname]                
               end
             end
           end
