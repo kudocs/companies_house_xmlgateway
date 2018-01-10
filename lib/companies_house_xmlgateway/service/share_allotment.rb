2240ea8bd85835a391d0d0e84bc5b9d27509bf9b
davidraj <davidraj.samuel@gmail.com>
2018-01-10 11:47:03 +0000

    error in test

--- lib/companies_house_xmlgateway/service/share_allotment.rb
+++ lib/companies_house_xmlgateway/service/share_allotment.rb
@@ -2,15 +2,18 @@ module CompaniesHouseXmlgateway
   module Service
     class ShareAllotment < CompaniesHouseXmlgateway::Service::Form
       API_CLASS_NAME = 'ReturnofAllotmentShares'
-      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v1-0/schema/forms/ReturnofAllotmentShares-v3-0.xsd'
+      SCHEMA_XSD = 'http://xmlalpha.companieshouse.gov.uk/v1-0/schema/forms/ReturnofAllotmentShares-v3-0-rc2.xsd'
+      #SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v1-0/schema/forms/ReturnofAllotmentShares-v3-0.xsd'
       
       # Generate the XML document that is embedded in the envelope for a Secretary Appointment action
       def build(submission)
         super do |xml|
           xml.ReturnofAllotmentShares(
-            'xmlns' => 'http://xmlgw.companieshouse.gov.uk',
+            #'xmlns' => 'http://xmlgw.companieshouse.gov.uk',
+            'xmlns' => 'http://xmlalpha.companieshouse.gov.uk',
             'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-            'xsi:schemaLocation' => "http://xmlgw.companieshouse.gov.uk #{SCHEMA_XSD}"
+            #'xsi:schemaLocation' => "http://xmlgw.companieshouse.gov.uk #{SCHEMA_XSD}"
+            'xsi:schemaLocation' => "http://xmlalpha.companieshouse.gov.uk/v1-0/xmlgw/Gateway #{SCHEMA_XSD}"
           ) do
 
             xml.StartPeriodSharesAllotted submission.data[:start_date]
