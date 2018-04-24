866b1d017980de23d73f71ae3e769c533fb73fa2
davidraj <davidraj.samuel@gmail.com>
2018-04-24 17:28:50 +0100

    version for allotment of shares

--- lib/companies_house_xmlgateway/service/share_allotment.rb
+++ lib/companies_house_xmlgateway/service/share_allotment.rb
@@ -3,8 +3,8 @@ module CompaniesHouseXmlgateway
     class ShareAllotment < CompaniesHouseXmlgateway::Service::Form
       API_CLASS_NAME = 'ReturnofAllotmentShares'
       #SCHEMA_XSD = 'http://xmlalpha.companieshouse.gov.uk/v1-0/schema/forms/ReturnofAllotmentShares-v3-0-rc2.xsd'
-      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v2-1/schema/forms/ReturnofAllotmentShares-v3-0.xsd'
-      #SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v2-1/schema/forms/ReturnofAllotmentShares-v2-5-1.xsd'
+      #SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v2-1/schema/forms/ReturnofAllotmentShares-v3-0.xsd'
+      SCHEMA_XSD = 'http://xmlgw.companieshouse.gov.uk/v2-1/schema/forms/ReturnofAllotmentShares-v2-5-1.xsd'
       
       # Generate the XML document that is embedded in the envelope for a Secretary Appointment action
       def build(submission)
