5de6eb5538f894800af0c6e5c5600b7e7dbb65ab
davidraj <davidraj.samuel@gmail.com>
2020-02-12 11:33:12 +0000

    The authentication information are missing for the document submit

--- lib/companies_house_xmlgateway/service/accounts.rb
+++ lib/companies_house_xmlgateway/service/accounts.rb
@@ -10,23 +10,26 @@ module CompaniesHouseXmlgateway
             'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
             'xsi:schemaLocation' => 'http://xmlgw.companieshouse.gov.uk/Header http://xmlgw.companieshouse.gov.uk/v2-1/schema/forms/FormSubmission-v2-9.xsd'
           ) do
-            xml.FormHeader do             
-              xml.CompanyName submission.company[:name]              
+            xml.FormHeader do
+              xml.CompanyNumber submission.company[:number]
+              xml.CompanyType submission.company[:type]
+              xml.CompanyName submission.company[:name]
+              xml.CompanyAuthenticationCode submission.company[:auth_code]
               xml.PackageReference PACKAGE_REFERENCE
-              xml.FormIdentifier 'Accounts'#self.class::API_CLASS_NAME
-              xml.SubmissionNumber submission.id              
+              xml.FormIdentifier 'Accounts'
+              xml.SubmissionNumber submission.id
+              xml.ContactName submission.company[:contact_name]
+              xml.ContactNumber submission.company[:contact_number]
             end
             xml.DateSigned Date.today.to_s
             xml.Form do
-            end
-            
+            end            
               xml.Document do
                 xml.Data submission.data[:doc_data]
                 xml.Filename 'accounts'
                 xml.ContentType "application/pdf"
                 xml.Category 'ACCOUNTS'
-              end
-            
+              end            
           end          
         end
       end
